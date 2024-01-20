0da9261513295edf0ecefe61b48b3f43ca9b1e4d
HRDs-VERSE <hrds.verse@gmail.com>
2024-01-20 23:11:20 +0530

    Tweets aggregation pipeline written fro getUserTweets

--- project/src/controllers/tweet.controller.js
+++ project/src/controllers/tweet.controller.js
@@ -1,9 +1,9 @@
 import mongoose, { isValidObjectId } from "mongoose"
-import {Tweet} from "../models/tweet.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { Tweet } from "../models/tweet.model.js"
+import { User } from "../models/user.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     const { content } = req.body
@@ -28,7 +28,72 @@ const createTweet = asyncHandler(async (req, res) => {
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
-    
+    const { userId } = req.params
+
+    if (!userId) {
+        throw new ApiError(400, "Something went wrong while getting User Id")
+    }
+
+    const user = await User.findById(userId)
+
+    if (!user) {
+        throw new ApiError(404, "No user found by this Id")
+    }
+
+
+    const userTweets = await Tweet.aggregate([
+
+        {
+            $match: {
+              owner: new mongoose.Types.ObjectId("659d5aa2d9dbfa281c83526f")
+            }
+          },
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "User"
+            }
+          },
+          {
+            $unwind: "$User"
+          },
+          {
+            $group: {
+              _id: null,
+              content: { $push: "$content" },
+              User: { $first: "$User" }
+            }
+          },
+          {
+            $project: {
+              _id: 0,
+              content: 1,
+              User: {
+                fullName: 1,
+                avatar: 1,
+                email: 1,
+                username: 1
+              }
+            }
+          }
+    ])
+
+    if (userTweets.length === 0) {
+        return res.status(404).json(
+            new ApiResponse(
+                404,
+                null,
+                `No Tweets found for User ${user.username}`
+            )
+        );
+    }
+    return res
+        .status(200)
+        .json(new ApiResponse(200, userTweets, "Tweets fetched successfully"))
+
+
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
@@ -56,11 +121,11 @@ const updateTweet = asyncHandler(async (req, res) => {
     const updatedTweet = await Tweet.findByIdAndUpdate(
         tweetId,
         {
-            $set:{
+            $set: {
                 content
             }
         },
-        {new: true}
+        { new: true }
     );
 
 
