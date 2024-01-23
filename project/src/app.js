0ddae84adc0ce31b0eec1833733e73b29203667a
HRDs-VERSE <hrds.verse@gmail.com>
2024-01-23 11:12:33 +0530

    All routes add

--- project/src/app.js
+++ project/src/app.js
@@ -21,6 +21,9 @@ import videoRouter from "./routes/video.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import commentRouter from "./routes/comment.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import playlist from "./routes/playlist.routes.js"
+import dashboard from "./routes/dashboard.routes.js"
 
 // routes declaration
 app.use("/api/v1/users", userRouter)
@@ -28,6 +31,9 @@ app.use("/api/v1/video", videoRouter)
 app.use("/api/v1/tweet", tweetRouter)
 app.use("/api/v1/like", likeRouter)
 app.use("/api/v1/comment", commentRouter)
+app.use("/api/v1/subscribe", subscriptionRouter)
+app.use("/api/v1/playlist", playlist)   
+app.use("/api/v1/dashboard", dashboard)
 
 
 export {app}
